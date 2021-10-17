c44032e480f9c6d21448d0a81761c4bb2d6f161f
taehy <taehy@DESKTOP-SPAA5HF>
2021-10-17 18:13:13 +0900

    mypageController_김태현A수정

--- FRECO/src/main/java/mypage/controller/MypagMemberUpdateController.java
+++ FRECO/src/main/java/mypage/controller/MypagMemberUpdateController.java
@@ -1,11 +1,9 @@
 package mypage.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import member.model.MemberDao;
 
 @Controller
 public class MypagMemberUpdateController {
