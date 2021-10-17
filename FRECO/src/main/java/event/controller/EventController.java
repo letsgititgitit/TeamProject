0dd1399a82585e266735a87d5b7bf929a9c7ddad
taehy <taehy@DESKTOP-SPAA5HF>
2021-10-17 18:11:55 +0900

    EventController__김태현A수정

--- FRECO/src/main/java/event/controller/EventController.java
+++ FRECO/src/main/java/event/controller/EventController.java
@@ -1,19 +1,40 @@
 package event.controller;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventController {
 	private final String command = "/event.ev";
-	private final String gotoPage = "event";
+	private final String getPage = "event";
+	
 	
 	@RequestMapping(value=command)
 	public String doAction() {
-		return gotoPage;
+		return getPage;
 	}
 	
+	@RequestMapping(value="eventToMypage.ev", method=RequestMethod.GET)
+	public ModelAndView doAction2(HttpSession session,ModelAndView mav) {
+
+		//로그인 X
+		if(session.getAttribute("loginInfo")==null) {
+			session.setAttribute("destination", "redirect:/mypageOrderList.mp");
+			mav.setViewName("redirect:/login.me");
+			return mav;
+		}
+		//로그인 O
+		else {
+				mav.setViewName(getPage);
+				return mav;
+			
+		}
 	
+	}
 	
 }
