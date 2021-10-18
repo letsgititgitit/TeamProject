3865f7bbea32c3d2e678227c71f2df116b7daa8a
heaun <91296366+refmia@users.noreply.github.com>
2021-10-18 15:40:36 +0900

    하은: OrderBean수정(쿠폰, 포인트값추가)

--- FRECO/src/main/java/order/model/OrderBean.java
+++ FRECO/src/main/java/order/model/OrderBean.java
@@ -4,6 +4,8 @@ public class OrderBean {
 	private int OINVOICE;  //송장번호 1001 주문순서(번호)
 	private String OMID;   //주문회원아이디
 	private String OORDERDATE; //주문 날짜(결제일)
+	private int OUSECOUPON; //결제시 사용된 쿠폰
+	private int OUSEPOINT; //결제시 사용된 포인트
 	private String OREFUND; //환불여부 (환불안함 NO) (환불요청됨 YES) 
 	
 	public int getOINVOICE() {
@@ -23,6 +25,18 @@ public class OrderBean {
 	}
 	public void setOORDERDATE(String oORDERDATE) {
 		OORDERDATE = oORDERDATE;
+	}
+	public int getOUSECOUPON() {
+		return OUSECOUPON;
+	}
+	public void setOUSECOUPON(int oUSECOUPON) {
+		OUSECOUPON = oUSECOUPON;
+	}
+	public int getOUSEPOINT() {
+		return OUSEPOINT;
+	}
+	public void setOUSEPOINT(int oUSEPOINT) {
+		OUSEPOINT = oUSEPOINT;
 	}
 	public String getOREFUND() {
 		return OREFUND;
